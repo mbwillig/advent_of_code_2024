476ff6d05af80b5e58bd1bec6e5a857f1f85abb7
mbwillig <mbwillig@hotmail.com>
2024-12-03 08:49:39 +0100

    day 3

--- day_03/micheal/solution.py
+++ day_03/micheal/solution.py
@@ -1,14 +1,5 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+import re
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -23,7 +14,26 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    txt = read_input_text()
+    pat = r"mul\((\d{1,3}),(\d{1,3})\)"
+    print(sum([int(a) * int(b) for a,b in re.findall(pat,txt)]))
 
 def part_b():
-    pass
+    txt = read_input_text()
+    pats = [r"mul\((\d{1,3}),(\d{1,3})\)",
+            r"(do\(\))",
+            r"(don't\(\))"]
+    results = sorted([res for pat in pats for res in re.finditer(pat,txt)], key = lambda x: x.start())
+
+    ans = 0
+    multiplier = 1
+    for result in results:
+        match result.groups():
+            case (a,b):
+                ans += int(a) * int(b) * multiplier
+            case (r'do()',):
+                multiplier = 1
+            case (r"don't()",):
+                multiplier = 0
+
+    print(ans)
