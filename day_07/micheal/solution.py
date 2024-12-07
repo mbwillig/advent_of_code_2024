ba0d3633006065293f02ba845939ddf650f20d64
mbwillig <mbwillig@hotmail.com>
2024-12-07 07:57:11 +0100

    day 7

--- day_07/micheal/solution.py
+++ day_07/micheal/solution.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import math
 import datetime
-import operator
+from operator import __mul__, __add__
 from copy import deepcopy
 from collections import Counter, ChainMap, defaultdict, deque
 from itertools import cycle
@@ -22,8 +22,28 @@ def read_input_text():
         return fh.read().strip()
 
 
-def part_a():
-    pass
+def solve_part(ops = [__mul__, __add__]):
 
-def part_b():
-    pass
+    def solve(goal,args, ix, total_so_far):
+        if (ix == len(args)):
+            return total_so_far == goal
+        for op in ops:
+            next_total = op(total_so_far,args[ix])
+            if next_total <= goal:
+                ans = solve(goal,args,ix +1, next_total)
+                if ans:
+                    return True
+        return False
+
+    ans = 0
+    for line in read_input_lines():
+        goal, args = line.split(":")
+        goal = int(goal)
+        args = [int(x) for x in args.strip().split()]
+        if solve(goal,args,1,args[0]):
+            ans+=goal
+
+    print(ans)
+
+solve_part()
+solve_part(ops = [__mul__, __add__, lambda x, y: int(str(x) + str(y))])
